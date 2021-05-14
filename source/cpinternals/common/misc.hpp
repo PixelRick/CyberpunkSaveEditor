5100dcb25339a058c07b24e7e014b693e3a45a42
PixelRick <thispawn@gmail.com>
2021-05-14 04:41:09 +0200

    misc.hpp (formatting + opti)

--- source/cpinternals/common/misc.hpp
+++ source/cpinternals/common/misc.hpp
@@ -10,114 +10,120 @@ namespace cp {
 struct task_progress
 {
 protected:
-	struct sub_progress
-	{
-		std::string comment;
-		float min;
-		float max;
-	};
+  struct sub_progress
+  {
+    std::string comment;
+    float min;
+    float max;
+  };
 
 public:
-	task_progress() noexcept
-		: main({ "processing...", 0.f, 1.f })
-	{
-	}
+  task_progress() noexcept
+    : main({ "processing...", 0.f, 1.f })
+  {
+  }
 
-	void push_max(float value)
-	{
-		const float clamped = std::clamp(value, m_value, 1.f);
-		auto& subp = current_sub_progress();
-		stack.emplace_back(sub_progress{ subp.comment, m_value, clamped });
-	}
+  void push_max(float value)
+  {
+    const float clamped = std::clamp(value, m_value, 1.f);
+    auto& subp = current_sub_progress();
+    stack.emplace_back(sub_progress{ subp.comment, m_value, clamped });
+  }
 
-	float pop_max()
-	{
-		if (stack.size())
-		{
-			m_value = stack.back().max;
-			stack.pop_back();
-			return m_value;
-		}
-		// should throw
-	}
+  float pop_max()
+  {
+    if (stack.size())
+    {
+      m_value = stack.back().max;
+      stack.pop_back();
+      return m_value;
+    }
+    // should throw
+  }
 
-	void set_comment(std::string comment)
-	{
-		auto& subp = current_sub_progress();
-		subp.comment = comment;
-	}
+  void set_comment(std::string comment)
+  {
+    auto& subp = current_sub_progress();
+    subp.comment = comment;
+  }
 
-	std::string comment()
-	{
-		auto& subp = current_sub_progress();
-		return subp.comment;
-	}
+  std::string comment()
+  {
+    auto& subp = current_sub_progress();
+    return subp.comment;
+  }
 
-	void advance(float value)
-	{
-		const float clamped = std::clamp(value, 0.f, 1.f);
-		auto& subp = current_sub_progress();
-		m_value = subp.min + (subp.max - subp.min) * clamped;
-	}
+  void advance(float value)
+  {
+    const float clamped = std::clamp(value, 0.f, 1.f);
+    auto& subp = current_sub_progress();
+    m_value = subp.min + (subp.max - subp.min) * clamped;
+  }
 
-	float normalized_value() const
-	{
-		return m_value;
-	}
-
-protected:
-
-	sub_progress& current_sub_progress()
-	{
-		if (stack.size())
-		{
-			return stack.back();
-		}
-		return main;
-	}
+  float normalized_value() const
+  {
+    return m_value;
+  }
 
 protected:
 
-	float m_value = 0.f;
-	std::vector<sub_progress> stack;
-	sub_progress main;
+  sub_progress& current_sub_progress()
+  {
+    if (stack.size())
+    {
+      return stack.back();
+    }
+    return main;
+  }
+
+protected:
+
+  float m_value = 0.f;
+  std::vector<sub_progress> stack;
+  sub_progress main;
 };
 
 // todo: deprecate
 struct progress_t
 {
-	float value = 0.f;
-	std::string comment;
+  float value = 0.f;
+  std::string comment;
 };
 
 // To replace exceptions, return this instead of a bool !
 struct op_status
 {
-	op_status() noexcept = default;
+  op_status() noexcept = default;
 
-	op_status(const std::string_view& err)
-		: m_err(err) {}
+  op_status(const std::string_view& err)
+    : m_err(err) {}
 
-	op_status(std::string err)
-		: m_err(err) {}
+  op_status(std::string err)
+    : m_err(err) {}
 
-	op_status(bool ok)
-		: m_err(ok ? "" : "uncommented error") {}
+  op_status(bool ok)
+  {
+    if (!ok)
+    {
+      m_err = "uncommented error";
+    }
+  }
 
-	op_status& operator=(const op_status& other) = default;
+  op_status& operator=(const op_status& other) = default;
 
-	operator bool() const
-	{
-		return m_err.empty();
-	}
+  // returns true when status is "ok"
+  operator bool() const
+  {
+    return !m_err.has_value();
+  }
 
-	std::string err() const
-	{
-		return m_err;
-	}
+  std::string err() const
+  {
+    return m_err.value_or("");
+  }
 
 protected:
-	std::string m_err;
+  std::optional<std::string> m_err;
 };
 
 } // namespace cp
