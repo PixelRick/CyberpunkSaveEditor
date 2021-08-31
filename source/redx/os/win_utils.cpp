6376132153aa18a9b810798bc68a1713ba29d406
PixelRick <thispawn@gmail.com>
2021-08-31 23:56:39 +0200

    win macro fix

--- source/redx/os/win_utils.cpp
+++ source/redx/os/win_utils.cpp
@@ -1,5 +1,9 @@
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
+#ifndef WIN32_LEAN_AND_MEAN
+  #define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+  #define NOMINMAX
+#endif
 #include <Windows.h>
 
 #include <optional>
