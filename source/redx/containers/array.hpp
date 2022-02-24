488cc9f2b5f4b989162f5c61c04b1b7789b1afaf
PixelRick <thispawn@gmail.com>
2022-02-24 01:49:13 +0100

    redx::array template helpers

--- source/redx/containers/array.hpp
+++ source/redx/containers/array.hpp
@@ -8,5 +8,39 @@ namespace redx {
 template <typename T, size_t Size>
 using array = std::array<T, Size>;
 
+
+template <typename T>
+struct is_array
+  : std::false_type {};
+
+template <typename T, size_t N>
+struct is_array<T[N]>
+  : std::true_type {};
+
+template <typename T, size_t N>
+struct is_array<redx::array<T, N>>
+  : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_array_v = is_array<T>::value;
+
+
+template <typename T>
+struct array_traits_t;
+
+template <typename T, size_t N>
+struct array_traits_t<T[N]>
+{
+  using element_type = remove_cvref_t<T>;
+  static constexpr size_t element_count = N;
+};
+
+template <typename T, size_t N>
+struct array_traits_t<redx::array<T, N>>
+{
+  using element_type = remove_cvref_t<T>;
+  static constexpr size_t element_count = N;
+};
+
 } // namespace redx
 
